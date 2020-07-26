c8ea3e301424400e107983dc2e69e2572801bea8
ThomasDerZweifler <thomasfunke71@googlemail.com>
2020-07-26 20:45:56 +0200

    tetris

--- thomas/imageProcessing/main.py
+++ thomas/imageProcessing/main.py
@@ -8,14 +8,14 @@ print( str(im.width) + ", " + str(im.height))
 
 im.show()
 
-#imgProc.invert(im).show()
+imgProc.invert(im).show()
 
 imgProc.gray(im).show()
 
-#imgProc.alpha(im, 100).show()
+imgProc.alpha(im, 100).show()
 
-#imgProc.interlace(im).show()
+imgProc.interlace(im).show()
 
-#imgProc.reduce(im).show()
+imgProc.reduce(im).show()
 
 imgProc.halftoning(im).show()
