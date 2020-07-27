0b2b05fe4ff013c14de8edd7e2a62fa7fe84f8f0
ThomasDerZweifler <thomasfunke71@googlemail.com>
2020-07-27 23:07:04 +0200

    gtMain

---
+++ thomas/imageProcessing/qtMain.py
@@ -0,0 +1,75 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QMenu, QTextEdit, QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtGui import QIcon
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 file dialogs - pythonspot.com'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
+    
+    def initUI(self):
+        textEdit = QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        # get icons from here:  https://material.io/resources/icons/?icon=perm_media&style=baseline
+
+        openAct = QAction(QIcon('icon/open_folder.png'), 'Open', self)
+        openAct.setShortcut('Ctrl+O')
+        openAct.setStatusTip('Open Image')
+        openAct.triggered.connect(self.openFileNameDialog)
+
+        exitAct = QAction(QIcon('icon/power.png'), 'Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(self.close)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAct)
+
+        toolbar = self.addToolBar('Tools')
+        toolbar.addAction(openAct)
+        toolbar.addAction(exitAct)
+
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        #self.openFileNameDialog()
+        #self.openFileNamesDialog()
+        #self.saveFileDialog()
+        
+        self.show()
+    
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        if fileName:
+            print(fileName)
+    
+    def openFileNamesDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)", options=options)
+        if files:
+            print(files)
+    
+    def saveFileDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
+        if fileName:
+            print(fileName)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
