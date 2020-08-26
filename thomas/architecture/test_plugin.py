302dc53de85fc720a66ffa7d18ae249f4c28a151
ThomasDerZweifler <thomasfunke71@googlemail.com>
2020-08-26 10:15:23 +0200

    test

--- thomas/architecture/test_plugin.py
+++ thomas/architecture/test_plugin.py
@@ -9,5 +9,9 @@ class TestPlugin(unittest.TestCase):
         c = getattr(module,"Plugin")
         self.assertEqual(c().version(),"1.0.0")
 
+        module = __import__("plugin.moduleB", fromlist=[''])
+        c = getattr(module,"Plugin")
+        self.assertFalse(c().version() == "1.0.0")
+
 if __name__ == '__main__':
     unittest.main()
