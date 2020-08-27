6ea927cf56076014b99b75c691d569d85b730ac6
Thomas Funke <thomasfunke71@googlemail.com>
2020-08-27 15:49:28 +0200

    install xlrd

--- thomas/excel/Main.py
+++ thomas/excel/Main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+
+# sudo apt-get install python3-xlrd
+
 # index_col=0 tells pandas that column 0 is the index and not data 
 dfs = pd.read_excel('test.xlsx', sheet_name='Tabelle1')
 print(dfs.head())
