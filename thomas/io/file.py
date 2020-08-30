c9ab4cb38c66a2368f279a709fb7086edc4c0b00
ThomasDerZweifler <thomasfunke71@googlemail.com>
2020-08-30 10:52:52 +0200

    wordcloud

--- thomas/io/file.py
+++ thomas/io/file.py
@@ -1,5 +1,9 @@
-f = open("ringelnatz.txt", "r")
+#f = open("ringelnatz.txt", "r")
+f = open("lorem.txt", "r")
 
+# https://www.loremipsum.de/
+
+wordscloud = {}
 words = []
 
 for line in f.readlines():
@@ -8,9 +12,14 @@ for line in f.readlines():
         word = word.replace('(','').replace(')','')
         if len(word) > 1:
             words.append(word)
-
+            if word in wordscloud:
+                wordscloud[word] = wordscloud[word] +1
+            else:
+                wordscloud[word] = 1
 f.close()
 
 print("words: " + str(words))
 
-print("Wortzahl: " + str(len(words)))
+print("words count: " + str(len(words)))
+
+print("words cloud: " + str(wordscloud))
