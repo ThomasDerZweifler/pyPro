eae2cda23882dd0a946ef3e6032ab1dcdfedcaea
ThomasDerZweifler <thomasfunke71@googlemail.com>
2020-09-04 16:35:52 +0200

    sql

---
+++ thomas/database/main.py
@@ -0,0 +1,6 @@
+# Mongo DB : https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x/
+# PythonMongo: https://www.w3schools.com/python/python_mongodb_getstarted.asp
+
+# mySQL DB: https://dev.mysql.com/downloads/mysql/
+# PythonMySQL: https://www.w3schools.com/python/python_mysql_getstarted.asp
+
