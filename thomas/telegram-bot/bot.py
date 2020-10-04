765f19de0d9d5c2c5a0e07c9730e3a841b7860bf
ThomasDerZweifler <thomasfunke71@googlemail.com>
2020-10-04 14:04:59 +0200

    telegram bot

---
+++ thomas/telegram-bot/bot.py
@@ -0,0 +1,35 @@
+# pip3 install python-telegram-bot
+
+# https://www.freecodecamp.org/news/learn-to-build-your-first-bot-in-telegram-with-python-4c99526765e4/
+
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import requests # http
+import re #regular expressions
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()    
+    url = contents['url']
+    print(url)
+    return url
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater('1382819143:AAH7OeZ7INgU5tPA0ns3pAAnKOIn8tFoNm0')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop',bop))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
