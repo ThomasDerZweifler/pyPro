c8894f2ec485152918cecd99b30ad981d2511680
ThomasDerZweifler <thomasfunke71@googlemail.com>
2020-11-02 08:40:43 +0100

    essentials

---
+++ thomas/essentials/loops.py
@@ -0,0 +1,24 @@
+print("start")
+
+y = 1
+x = 0
+while y < 10 :
+
+    while x < 10 :
+
+        # Berechnung nach continue erzeugt innere Endlosschleife !
+        x = x + 1
+
+        # skip all even x
+        if x % 2 == 0 : continue 
+
+        # skip from x = 3
+        if x == 3 : break
+
+        print("innere Schleife x = {0}, y = {1}".format(x,y))
+    
+    print("äußere Schleife")
+    x = 0
+    y = y + 1
+
+print("end")
