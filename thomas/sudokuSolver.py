3775559c0516314d39b613e54bcd8cb75154d4da
thomasfunke <thomasfunke71@gmail.com>
2024-10-01 16:45:38 +0200

    solver

--- thomas/sudokuSolver.py
+++ thomas/sudokuSolver.py
@@ -140,13 +140,49 @@ def sudoku(size):
     total_time = time.time()-start_time
     return mydict, n, total_time
 
+def sudokuGenerator2():
+    from random import sample
+
+    base  = 3
+    side  = base*base
+
+    # pattern for a baseline valid solution
+    def pattern(r,c): return (base*(r%base)+r//base+c)%side
+
+    # randomize rows, columns and numbers (of valid base pattern)
+    from random import sample
+    def shuffle(s): return sample(s,len(s)) 
+    rBase = range(base) 
+    rows  = [ g*base + r for g in shuffle(rBase) for r in shuffle(rBase) ] 
+    cols  = [ g*base + c for g in shuffle(rBase) for c in shuffle(rBase) ]
+    nums  = shuffle(range(1,base*base+1))
+
+    # produce board using randomized baseline pattern
+    board = [ [nums[pattern(r,c)] for c in cols] for r in rows ]
+
+    for line in board: print(line)
+
+    squares = side*side
+    empties = squares * 3//4
+    for p in sample(range(squares),empties):
+        board[p//side][p%side] = 0
+
+    print("\n")
+    for line in board: print(line)
+
+    solve(board)
+    print("\n")
+    for line in board: print(line)
+
+sudokuGenerator2()
+
 def sudokuGegerator():
     return_dict, total_tries, amt_of_time = sudoku(9)
     print ('generated sudoku',total_tries,'tries in', round(amt_of_time,2), 'secs :\n')
     for v in return_dict.items():
         print(v)
 
-sudokuGegerator()
+# sudokuGegerator()
 
 def sudokuSolver():
     import time
